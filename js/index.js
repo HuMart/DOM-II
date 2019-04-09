38cfa27e493e04cb986d71656cadc934858fe325
Manya0 <manya01@protonmail.com>
2019-04-09 17:48:12 -0400

    Hard

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+//mouseover and out
+
+let navLogo = document.querySelector('.logo-heading');
+navLogo.addEventListener("mouseover", (event) => event.target.style.transform = "scale(2)");
+navLogo.addEventListener("mouseout", (event) => event.target.style.transform = "scale(1)");
+
+//dblclick
+
+let navTag = document.querySelector('.nav');
+navTag.addEventListener('dblclick', (event) => event.target.style.backgroundColor = 'yellow');
+
+//keydown and up
+
+let body = document.querySelector('body');
+body.addEventListener('keydown', (event) => event.target.style.backgroundColor = 'red');
+body.addEventListener('keyup', (event) => event.target.style.backgroundColor = 'white');
+
+//wheel
+
+body.addEventListener('scrol', function alertMeOnce(){
+    alert("SCROLING!!!!!");
+    body.removeEventListener('scrol', alertMeOnce);
+  });
+
+// drag
+
+let BusBanner = document.querySelector(".intro > img" );
+BusBanner.addEventListener('drag' ,(event) => {
+    event.target.style.transform = 'scale(0.75)'
+})
+
+let header2 = document.querySelectorAll('.home .text-content h2');
+let buttons = document.querySelectorAll('.btn');
+
+for (i = 0; i < header2.length; i++) {
+    header2[i].addEventListener('select', function(e) {
+        e.target.style.color = 'red';
+        });
+    }
+ 
+for (i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(e) {
+    e.target.style.background = 'red';
+    e.target.style.color = 'green';
+    body.classList.toggle('container-background');
+    });
+}
+  
+
+
